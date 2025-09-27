c7125220fe0ac57af8798112dd7165cc438a0375
Spitball1234 <liam.jbm2020@gmail.com>
2025-09-27 14:01:43 -0400

    Update main.java

--- main.java
+++ main.java
@@ -3,20 +3,25 @@ import java.lang.Math
 public class Main() {
     // this is a internal sqrt function NOT fully functional
     private float sqrt(float sqrtable) {
-        iter = 1;
-        sqrd = iter*iter;
+        int iter = 1;
+        int sqrd = iter*iter;
         while (sqrd < sqrtable && sqrd != sqrtable) {
             iter += 1;
             sqrd = iter*iter;
         }
+        // this is a hack to make code work so my sqrt() can be simple
         if (sqrd != sqrtable) {
+            // if the returned value contains a decimal, the check will fail
             return sqrd - 0.9;
         } else {
+            // otherwise, the check succeeds
             return sqrd;
         }
     }
     private int floor(float rndable) {
-        return Math.floor(rndable);
+        // this is a hack to rnd down by using javas float to int cast to truncate
+        int rnded = (int) rndable
+        return rnded;
     }
     public string Main(float isperfect) {
         if (isperfect != floor(isperfect)) {
